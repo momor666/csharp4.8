22e7793a3795dbe70670ea6a9811b1ede5cbbca6
ks7788 <1171200+ks7788@users.noreply.github.com>
2019-05-01 09:30:19 -0500

    added

---
+++ binary search.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+public class Node
+{
+    public int Value { get; set; }
+
+    public Node Left { get; set; }
+
+    public Node Right { get; set; }
+
+    public Node(int value, Node left, Node right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+}
+
+public class BinarySearchTree
+{
+    public static bool Contains(Node root, int value)
+    {
+        if (root.Value == value) return true;
+
+        if (root.Left != null && value < root.Value)
+          if (Contains(root.Left, value)) return true;
+
+        if (root.Right != null && value > root.Value)
+            if (Contains(root.Right, value)) return true;
+
+        return false;
+    }
+
+    public static void Main(string[] args)
+    {
+        Node n1 = new Node(1, null, null);
+        Node n3 = new Node(3, null, null);
+        Node n2 = new Node(2, n1, n3);
+
+        Console.WriteLine(Contains(n2, 3));
+    }
+}
